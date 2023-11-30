22bb5cf6571cc416288a8266b65efff8ea2b7fab
ssungyeee <149459020+ssungyeee@users.noreply.github.com>
2023-11-30 17:14:37 +0900

    플레이어 움직임, 에임 추가
    하이어라키 추가
    Player1 - Player1Sprite - Player1AimPivot - Player1Weapon
    
    인풋 시스템 추가
    PlayerController
    
    스크립트 추가
    AimRotation
    Movement
    PlayerController
    PlayerInputController

---
+++ Assets/Scripts/AimRotation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimRotation : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _armRenderer;
+    [SerializeField] private Transform _armPivot;
+
+    [SerializeField] private SpriteRenderer _playerRenderer;
+
+    private PlayerController _controller;
+    private void Awake()
+    {
+        _controller = GetComponent<PlayerController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller.OnLookEvent += OnaAim;
+    }
+    public void OnaAim(Vector2 newAimDirection)
+    {
+        RotateArm(newAimDirection);
+    }
+
+    private void RotateArm(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        _armRenderer.flipY = Mathf.Abs(rotZ) > 90f;
+        _playerRenderer.flipX = _armRenderer.flipY;
+
+        _armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+    }    
+}
