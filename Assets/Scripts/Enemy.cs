0e6c75c5f86fed274f4ff4880ca7081f4899f2e2
Chhhhong <101609335+Chhhhong@users.noreply.github.com>
2023-12-01 15:30:19 +0900

    EnemyScript#1
    create Enemy Hp Bar
    EnemyScript
    - Enemy Random Position
    - Enemy Hp
    - get damaged

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float _hp;
+    private float _damage = 0f;
+    public int type;
+
     void Start()
     {
-        
+        // 랜덤한 좌표 가져오기
+        float x = Random.Range(-8f, 8f);
+        float y = 10f;
+
+        // 몬스터의 타입이 1일때 체력 10f
+        if (type == 1)
+        {
+            _hp = 10f;
+        }
+        // 몬스터의 타입이 2일때 체력 20f
+        else if (type == 2)
+        {
+            _hp = 20f;
+        }
+        transform.position = new Vector3(x, y, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0, -0.05f, 0);
+    }
+
+    // Bullet과 Enemy가 부딪혔을때 발생하는 일들
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Bullet")
+        {
+            if (_damage < _hp)
+            {
+                _damage += 2f;
+                Destroy(collider.gameObject);
+                gameObject.transform.Find("Enemy1Sprite/Canvas/Front").transform.localScale = new Vector3(_damage / _hp, 1.0f, 1.0f);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
