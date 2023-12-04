ac143becfff44e08c401204b8dcc135e2ed31cdc
meongcode <147586074+meongcode@users.noreply.github.com>
2023-12-04 20:14:40 +0900

    GameManager edit
    플레이어,적 등장 기본
    플레이 시간 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,9 +7,19 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
-    public static GameObject playTime;
-    public PlayerController player;
+    public GameObject playTime;
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+    public GameObject enemy2Prefab;
+    public GameObject bossPrefab;
+    public GameObject enemyTypeTime;
 
+    private float enemySpawnTimer = 0f;
+    private float enemy2SpawnTimer = 0f;
+    private float spawnInterval = 5f;  // n초 간격 (예: 5초)
+
+    int currentMinutes;
+    int currentSeconds;
 
     private void Awake()
     {
@@ -23,4 +33,54 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    //------------------------------------------------------------------------
+    
+    private void Start()
+    {
+        InstantiatePlayer();
+        
+    }
+
+    
+    void Update()
+    {
+        currentMinutes = enemyTypeTime.GetComponent<PlayTime>().GetMinutes();
+        currentSeconds = enemyTypeTime.GetComponent<PlayTime>().GetSeconds();
+
+        if (currentMinutes >= 0 && currentMinutes < 1)
+        {
+            enemySpawnTimer += Time.deltaTime;
+            enemy2SpawnTimer += Time.deltaTime;
+
+            if (enemySpawnTimer >= spawnInterval)
+            {
+                Instantiate(enemyPrefab);
+                enemySpawnTimer = 0f;  // 타이머 초기화
+            }
+
+            if (currentSeconds >= 30 && enemy2SpawnTimer >= spawnInterval)
+            {
+                Instantiate(enemy2Prefab);
+                enemy2SpawnTimer = 0f;  // 타이머 초기화
+            }
+        }
+        else if (currentMinutes >= 1)
+        {
+            
+
+            if (currentSeconds == 0)
+            {
+                Destroy(enemyPrefab);
+                Destroy(enemy2Prefab);
+                Instantiate(bossPrefab);
+            }
+        }
+    }
+
+    private void InstantiatePlayer()
+    {
+        Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+    }
+    
+   
 }
