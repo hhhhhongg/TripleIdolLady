c427f0a8a8d74d34be48f60e384f8d34814d607d
levell1 <whddnr0317@naver.com>
2023-12-05 19:50:16 +0900

    [Add] 플레이어 카메라 이동

---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    Transform playerTransform;
+
+    void Awake()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        this.transform.position = playerTransform.position + new Vector3(0,0,-10);
+    }
+}
