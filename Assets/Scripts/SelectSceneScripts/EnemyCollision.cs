3e1739daf469bd2f4e091ec883c6b5b505186a86
levell1 <whddnr0317@naver.com>
2023-12-05 14:35:58 +0900

    [ADD] Audio

--- Assets/Scripts/SelectSceneScripts/EnemyCollision.cs
+++ Assets/Scripts/SelectSceneScripts/EnemyCollision.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class EnemyCollision : MonoBehaviour
 {
     // Update is called once per frame
+    public AudioSource audioSource;
 
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        audioSource.Play();
         Destroy(collision.gameObject);
     }
 }
